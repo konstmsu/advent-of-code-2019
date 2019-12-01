f15d3b34d0d26d4d21ef6bb25681d07ce9643969
Konstantin Spirin <konstmsu@gmail.com>
2019-12-02 04:11:55 +0800

    Day 1 solution

---
+++ day-01/solution.py
@@ -0,0 +1,19 @@
+#%%
+import numpy as np
+
+with open('day-01/input') as file:
+  masses = np.array([int(a) for a in file.readlines()])
+
+print(sum(masses // 3 - 2))
+# 3338004 is too high
+
+#%%
+total = np.zeros_like(masses)
+
+m = masses
+while np.any(m > 0):
+  m = m // 3 - 2
+  m[m < 0] = 0
+  total += m
+
+print(sum(total))
