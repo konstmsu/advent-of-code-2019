ed040b28c0533bce0821791b3d776e6c5090d533
Konstantin Spirin <konstmsu@gmail.com>
2019-12-03 22:56:08 +0800

    Day 3 part 2 done

--- day-03/solution.py
+++ day-03/solution.py
@@ -1,7 +1,17 @@
 # %%
 import numpy as np
+
+
+def parse_wires(lines):
+    return [l.split(',') for l in lines]
+
+
 with open('day-03/input') as file:
-    wires = [l.split(',') for l in file.readlines()]
+    wires = parse_wires(file.readlines())
+
+# wires = parse_wires(
+#     ["R75,D30,R83,U83,L12,D49,R71,U7,L72",
+#      "U62,R66,U55,R34,D71,R55,D58,R83"])
 
 # %%
 
@@ -33,6 +43,15 @@ def get_path(wire):
     return zip(x[1:], y[1:])
 
 
-points = [set(get_path(wire)) for wire in wires]
+paths = [list(get_path(wire)) for wire in wires]
+points = [set(path) for path in paths]
 crossings = points[0].intersection(points[1])
 print(min(abs(p[0]) + abs(p[1]) for p in crossings))
+
+# %%
+distances = [{path[i]:i+1 for i in range(len(path))} for path in paths]
+min([sum(d[c] for d in distances) for c in crossings])
+
+# 43846 is too low
+
+# %%
